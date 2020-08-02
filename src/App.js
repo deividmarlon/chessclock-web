5e06bfe0116482e8fefd53c7fc41d162dc2014eb
Deivid <deividmarlon@gmail.com>
2020-08-02 19:44:08 -0300

    fix lock clocks on time zero

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -2,13 +2,18 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 import './clock.css';
-
 import logo from '../../logo.svg';
 
 const Clock = props => {
-  console.log(props);
+
+  if((props.clockOne.m ===0 && props.clockOne.s ===0) || (props.clockTwo.m===0 && props.clockTwo.s===0)){
+    props.disableStatusOne = true;
+    props.disableStatusTwo = true;
+  }
+
   return (
-    <div className="ClockPage"> 
+    <>
+    <div className={"ClockPage"}> 
       <main>
           <button className={props.timerOneStyle} disabled={props.disableStatusOne} 
             onClick={()=>{
@@ -37,8 +42,12 @@ const Clock = props => {
           </button>
       </main>
     </div>
-  );
 
+    <div className={"Modal"}>
+    
+    </div>
+    </>
+  );
 }
 
 export default Clock;
